2d30e1923df3498d9ab47a978bcf6cd4fad6b973
aknafu12 <kgebre2021@gmail.com>
2023-05-08 20:03:23 +0300

    dijkstra algorith give start and end

---
+++ dijkstra_algo.py
@@ -0,0 +1,48 @@
+'''Here is a Python implementation of Dijkstra's algorithm to find the shortest path between two nodes in a graph. This code assumes that the graph is represented as a dictionary of dictionaries, where the keys are node names and the values are dictionaries containing connected nodes and their respective distances.
+'''
+import heapq
+
+def dijkstra(graph, start, end):
+    shortest_paths = {start: (None, 0)}
+    current_node = start
+    visited = set()
+
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph[current_node].keys()
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph[current_node][next_node] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "Route not possible"
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    path = path[::-1]
+    return path
+
+# Example graph represented as a dictionary of dictionaries
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+start = 'A'
+end = 'D'
+print("Shortest path from {} to {} is:".format(start, end), dijkstra(graph, start, end))
+
